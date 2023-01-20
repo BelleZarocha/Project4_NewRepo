4f3d265e66a1d3d54ea93372aa5c429d2d39627f
BelleZarocha <sarocha.pj@gmail.com>
2023-01-21 01:52:03 +0700

    Django Setup

---
+++ backend/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+# from .views import PackageList, PackageDetail
+
+app_name = 'api'
+
+urlpatterns = [
+    # path('<int:pk>/', PackageDetail.as_view(), name='packagedetail'),
+    # path('', PackageList.as_view(), name='packagelist'),
+]
