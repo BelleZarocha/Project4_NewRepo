50b1098b8a305617900716f31abe3ac0ab3ef5be
BelleZarocha <sarocha.pj@gmail.com>
2023-01-21 17:48:45 +0700

    Create Components

---
+++ frontend/frontend/src/components/Packages/Packages.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+	cardMedia: {
+		paddingTop: '56.25%', // 16:9
+	},
+	link: {
+		margin: theme.spacing(1, 1.5),
+	},
+	cardHeader: {
+		backgroundColor:
+			theme.palette.type === 'light'
+				? theme.palette.grey[200]
+				: theme.palette.grey[700],
+	},
+	packageTitle: {
+		fontSize: '16px',
+		textAlign: 'left',
+	},
+	packageText: {
+		display: 'flex',
+		justifyContent: 'left',
+		alignItems: 'baseline',
+		fontSize: '12px',
+		textAlign: 'left',
+		marginBottom: theme.spacing(2),
+	},
+}));
+
+const Packages = (props) => {
+	const { packages } = props;
+	const classes = useStyles();
+	if (!packages || packages.length === 0) return <p>Can not find any posts, sorry</p>;
+	return (
+		<React.Fragment>
+			<Container maxWidth="md" component="main">
+				<Grid container spacing={5} alignItems="flex-end">
+					{packages.map((pets_insure) => {
+						return (
+							// Enterprise card is full width at sm breakpoint
+							<Grid item key={pets_insure.id} xs={12} md={4}>
+								<Card className={classes.card}>
+									<CardMedia
+										className={classes.cardMedia}
+										image="https://source.unsplash.com/random"
+										title="Image title"
+									/>
+									<CardContent className={classes.cardContent}>
+										<Typography
+											gutterBottom
+											variant="h6"
+											component="h2"
+											className={classes.packageTitle}
+										>
+											{pets_insure.title.substr(0, 50)}...
+										</Typography>
+										<div className={classes.packageText}>
+											<Typography
+												component="p"
+												color="textPrimary"
+											></Typography>
+											<Typography variant="p" color="textSecondary">
+												{pets_insure.excerpt.substr(0, 60)}...
+											</Typography>
+										</div>
+									</CardContent>
+								</Card>
+							</Grid>
+						);
+					})}
+				</Grid>
+			</Container>
+		</React.Fragment>
+	);
+};
+export default Packages;
--- frontend/frontend/src/index.js
+++ frontend/frontend/src/index.js
@@ -3,15 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import * as serviceworker from './serviceworker';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+const routing = (
+  <Router>
+    <React.StrictMode>
+      <Header />
+      <Routes>
+        <Route exact path='/' components={App} />
+      </Routes>
+      <Footer />
+    </React.StrictMode>
+  </Router>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ReactDOM.render(routing, document.getElementById('root'));
+
+serviceworker.unregister();
 reportWebVitals();
